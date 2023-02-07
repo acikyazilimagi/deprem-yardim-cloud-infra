d2e05abc1e03766ecf50015be5a69270856c2825
Emre Ceyhan <a@emreceyhan.com>
2023-02-07 19:28:26 +0300

    worker service added

--- ecs.tf
+++ ecs.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "service-sg" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-    ingress {
+  ingress {
     description      = "HTTP"
     from_port        = 3000
     to_port          = 3000
@@ -41,8 +41,8 @@ resource "aws_security_group" "service-sg" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
- 
- 
+
+
 
   egress {
     from_port        = 0
@@ -57,3 +57,52 @@ resource "aws_security_group" "service-sg" {
     Environment = var.environment
   }
 }
+
+//worker
+resource "aws_ecs_task_definition" "worker-TD" {
+  family                   = "worker-TD"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 512
+  memory                   = 1024
+  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
+  container_definitions = jsonencode([
+    {
+      name   = "container-name"
+      image  = "nginx"
+      cpu    = 512
+      memory = 1024
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-create-group  = "true"
+          awslogs-group         = "/ecs/worker"
+          awslogs-region        = var.region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+      essential = true
+    }
+  ])
+
+}
+
+resource "aws_ecs_service" "worker-service" {
+  name            = "monolith-service"
+  cluster         = aws_ecs_cluster.base-cluster.id
+  task_definition = aws_ecs_task_definition.worker-TD.id
+  desired_count   = 1
+  depends_on = [
+    aws_ecs_cluster.base-cluster,
+    aws_ecs_task_definition.worker-TD,
+  ]
+  launch_type = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.private-subnet-a.id, aws_subnet.private-subnet-b.id]
+    security_groups  = [aws_security_group.service-sg.id]
+    assign_public_ip = true
+  }
+}
+
+//worker
