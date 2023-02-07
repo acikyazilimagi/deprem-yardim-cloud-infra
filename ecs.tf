fbffef7f1558f5a97a96016405af7cd628fa7594
Emre Ceyhan <a@emreceyhan.com>
2023-02-07 19:25:43 +0300

    ecs added

---
+++ ecs.tf
@@ -0,0 +1,59 @@
+data "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsServiceRole"
+}
+
+resource "aws_ecs_cluster" "base-cluster" {
+  name = "base-cluster"
+  tags = {
+    Name        = "base-cluster"
+    Environment = var.environment
+  }
+}
+
+
+
+resource "aws_security_group" "service-sg" {
+  name        = "service-sg"
+  description = "Allow HTTP/HTTPS inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    description      = "HTTP"
+    from_port        = 9000
+    to_port          = 9000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+    ingress {
+    description      = "HTTP"
+    from_port        = 3000
+    to_port          = 3000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+ 
+ 
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name        = "service-sg"
+    Environment = var.environment
+  }
+}
