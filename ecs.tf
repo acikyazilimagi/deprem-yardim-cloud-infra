8ca727ff17105b9e5207adf720e68d7d4b427f3f
Emre Ceyhan <a@emreceyhan.com>
2023-02-07 19:33:40 +0300

    telegram bot addded

--- ecs.tf
+++ ecs.tf
@@ -58,6 +58,57 @@ resource "aws_security_group" "service-sg" {
   }
 }
 
+
+//teelgram bot
+resource "aws_ecs_task_definition" "telegrambot-TD" {
+  family                   = "telegrambot-TD"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 512
+  memory                   = 1024
+  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
+  container_definitions = jsonencode([
+    {
+      name   = "container-name"
+      image  = "nginx"
+      cpu    = 512
+      memory = 1024
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-create-group  = "true"
+          awslogs-group         = "/ecs/telegrambot"
+          awslogs-region        = var.region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+      essential = true
+    }
+  ])
+
+}
+
+resource "aws_ecs_service" "tegilegrambot-service" {
+  name            = "monolith-service"
+  cluster         = aws_ecs_cluster.base-cluster.id
+  task_definition = aws_ecs_task_definition.telegrambot-TD.id
+  desired_count   = 1
+  depends_on = [
+    aws_ecs_cluster.base-cluster,
+    aws_ecs_task_definition.telegrambot-TD,
+  ]
+  launch_type = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.private-subnet-a.id, aws_subnet.private-subnet-b.id]
+    security_groups  = [aws_security_group.service-sg.id]
+    assign_public_ip = true
+  }
+}
+
+//teelgram bot
+
+
 //worker
 resource "aws_ecs_task_definition" "worker-TD" {
   family                   = "worker-TD"
@@ -106,3 +157,5 @@ resource "aws_ecs_service" "worker-service" {
 }
 
 //worker
+
+
