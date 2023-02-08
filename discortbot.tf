7403301c3d9d14b93020fb1697cd3ccc309a765a
Emre Ceyhan <a@emreceyhan.com>
2023-02-08 19:09:04 +0300

    discor bot

---
+++ discortbot.tf
@@ -0,0 +1,49 @@
+
+//worker
+resource "aws_ecs_task_definition" "discordbot-TD" {
+  family                   = "discordbot-TD"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 512
+  memory                   = 1024
+  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
+  container_definitions = jsonencode([
+    {
+      name   = "container-name"
+      image  = "nginx"
+      cpu    = 512
+      memory = 1024
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-create-group  = "true"
+          awslogs-group         = "/ecs/worker"
+          awslogs-region        = var.region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+      essential = true
+    }
+  ])
+
+}
+
+resource "aws_ecs_service" "discordbot-service" {
+  name            = "discordbot-service"
+  cluster         = aws_ecs_cluster.base-cluster.id
+  task_definition = aws_ecs_task_definition.discordbot-TD.id
+  desired_count   = 1
+  depends_on = [
+    aws_ecs_cluster.base-cluster,
+    aws_ecs_task_definition.discordbot-TD,
+  ]
+  launch_type = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.private-subnet-a.id, aws_subnet.private-subnet-b.id]
+    security_groups  = [aws_security_group.service-sg.id]
+    assign_public_ip = true
+  }
+}
+
+//worker
