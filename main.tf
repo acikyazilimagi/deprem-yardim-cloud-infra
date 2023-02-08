15214f39a9b9c021bb41bad9682ddb9c80f6c0bf
Emre Ceyhan <a@emreceyhan.com>
2023-02-08 22:44:08 +0300

    worker ecr added

--- main.tf
+++ main.tf
@@ -169,6 +169,44 @@ resource "aws_ecr_lifecycle_policy" "ecr-policy" {
 }
 EOF
 }
+
+
+
+
+
+resource "aws_ecr_repository" "worker-ecr" {
+  name                 = "worker-ecr"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "worker-policy" {
+  repository = aws_ecr_repository.worker-ecr.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 10 days",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 10
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
 /*
 
 module "iam" {
