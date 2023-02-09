123fd121c670b73ef2536a373095f598069abc13
kaanozbudak <kaan.ozbudak@ametrica.io>
2023-02-09 11:07:40 +0300

    load balancer 8080 up

--- alb.tf
+++ alb.tf
@@ -28,6 +28,15 @@ resource "aws_security_group" "backend-alb-sg" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  ingress {
+    description      = "HTTP"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
