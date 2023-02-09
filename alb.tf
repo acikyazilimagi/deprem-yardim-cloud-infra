d46f4d7a1e13c65ef94c0e495f3c22560ebc199d
Ferhat BOSTANCI <ferhatbostanciofficial@gmail.com>
2023-02-10 01:02:29 +0300

    depremio added

--- alb.tf
+++ alb.tf
@@ -132,7 +132,6 @@ resource "aws_lb_listener" "backend-go-alb-listener" {
 
 
 //------------- beniyiyim services ----------
-//alb
 resource "aws_lb" "beniyiyim-alb" {
   name               = "beniyiyim-alb"
   internal           = false
@@ -147,7 +146,6 @@ resource "aws_lb" "beniyiyim-alb" {
   }
 }
 
-//listener
 resource "aws_lb_listener" "beniyiyim-alb-listener" {
   load_balancer_arn = aws_lb.beniyiyim-alb.arn
   port              = "80"
@@ -161,5 +159,34 @@ resource "aws_lb_listener" "beniyiyim-alb-listener" {
     aws_lb.beniyiyim-alb
   ]
 }
-
 //------------- beniyiyim services ----------
+
+// ------------- depremio services ---------- \\
+resource "aws_lb" "depremio-alb" {
+  name               = "depremio-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.backend-alb-sg.id]
+  subnets            = [aws_subnet.public-subnet-a.id, aws_subnet.public-subnet-b.id]
+
+  enable_deletion_protection = true
+
+  tags = {
+    Name = "depremio-alb"
+  }
+}
+
+resource "aws_lb_listener" "depremio-alb-listener" {
+  load_balancer_arn = aws_lb.depremio-alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.depremio-tg.arn
+  }
+  depends_on = [
+    aws_lb.depremio-alb
+  ]
+}
+// ------------- depremio services ---------- \\
