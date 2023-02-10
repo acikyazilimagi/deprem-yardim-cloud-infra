61e73aff50e83436bc75a860ff79f9535810ad4b
Emre Ceyhan <a@emreceyhan.com>
2023-02-10 21:58:02 +0300

    eczane app added

--- alb.tf
+++ alb.tf
@@ -190,3 +190,35 @@ resource "aws_lb_listener" "depremio-alb-listener" {
   ]
 }
 // ------------- depremio services ---------- \\
+
+
+
+// ------------- eczane services ---------- \\
+resource "aws_lb" "eczane-alb" {
+  name               = "eczane-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["sg-09d6376212dfa6ea1"] // Todo change
+  subnets            = [aws_subnet.public-subnet-a.id, aws_subnet.public-subnet-b.id]
+
+  enable_deletion_protection = true
+
+  tags = {
+    Name = "eczane-alb"
+  }
+}
+
+resource "aws_lb_listener" "eczane-alb-listener" {
+  load_balancer_arn = aws_lb.eczane-alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.eczane-tg.arn
+  }
+  depends_on = [
+    aws_lb.eczane-alb
+  ]
+}
+// ------------- depremio services ---------- \\
---
+++ eczane.tf
@@ -0,0 +1,97 @@
+
+//eczane api
+
+
+resource "aws_ecs_task_definition" "eczane-TD" {
+  family                   = "eczane-TD"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 1024
+  memory                   = 2048
+  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
+  container_definitions = jsonencode([
+    {
+      name   = "container-name"
+      image  = "eczane" //bunu düzelticem
+      cpu    = 1024
+      memory = 2048
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-create-group  = "true"
+          awslogs-group         = "/ecs/eczane"
+          awslogs-region        = var.region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_lb_target_group" "eczane-tg" {
+  name        = "eczane-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.vpc.id
+  health_check {
+    enabled  = true
+    path     = "/api/health"
+    port     = 80
+    protocol = "HTTP"
+  }
+  tags = {
+    Name        = "eczane-tg"
+    Environment = var.environment
+  }
+}
+
+resource "aws_ecs_service" "eczane-service" {
+  name            = "eczane-service"
+  cluster         = aws_ecs_cluster.base-cluster.id
+  task_definition = aws_ecs_task_definition.eczane-TD.id
+  desired_count   = 2
+  depends_on = [
+    aws_ecs_cluster.base-cluster,
+    aws_ecs_task_definition.eczane-TD,
+    aws_lb_target_group.eczane-tg,
+  ]
+  launch_type = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.private-subnet-a.id, aws_subnet.private-subnet-b.id]
+    security_groups  = [aws_security_group.service-sg.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.eczane-tg.arn
+    container_name   = "container-name"
+    container_port   = 80
+  }
+}
+
+
+resource "aws_lb_listener_rule" "eczane-rule" {
+  listener_arn = aws_lb_listener.eczane-alb-listener.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.eczane-tg.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+}
+// eczane api
