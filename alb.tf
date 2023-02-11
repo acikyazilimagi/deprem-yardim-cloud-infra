ba19d27cc4c50b24df330757828d9bff9b67c0a1
kaanozbudak <kaan.ozbudak@ametrica.io>
2023-02-11 19:42:12 +0300

    go consumer service sg added and created

--- alb.tf
+++ alb.tf
@@ -59,6 +59,34 @@ resource "aws_security_group" "backend-alb-sg" {
 }
 /////////////////////////
 
+resource "aws_security_group" "allow-http-sg" {
+  name        = "allow-http-sg"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow-http-sg"
+  }
+}
+/////////////////////////
+
 
 //------------- backend services ----------
 //alb
