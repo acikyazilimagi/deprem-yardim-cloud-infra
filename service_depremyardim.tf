90e303159e1b8ed328456d4e28f6f830ee4202c6
Emre Erkunt <emre.erkunt@gmail.com>
2023-02-11 20:59:27 +0000

    Update on the service

--- service_depremyardim.tf
+++ service_depremyardim.tf
@@ -48,17 +48,6 @@ resource "aws_lb_target_group" "api-tg" {
   }
 }
 
-data "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsServiceRole"
-}
-
-resource "aws_ecs_cluster" "base-cluster" {
-  name = "base-cluster"
-  tags = {
-    Name = "base-cluster"
-  }
-}
-
 resource "aws_ecs_service" "api-service" {
   name            = "api-service"
   cluster         = aws_ecs_cluster.base-cluster.id
@@ -171,52 +160,5 @@ resource "aws_ecs_service" "worker-service" {
     subnets          = [aws_subnet.private-subnet-a.id, aws_subnet.private-subnet-b.id]
     security_groups  = [aws_security_group.service-sg.id]
     assign_public_ip = true
-  }
-}
-
-
-resource "aws_security_group" "service-sg" {
-  name        = "service-sg"
-  description = "Allow HTTP/HTTPS inbound traffic"
-  vpc_id      = aws_vpc.vpc.id
-
-  ingress {
-    description      = "HTTP"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-  ingress {
-    description      = "HTTP"
-    from_port        = 9000
-    to_port          = 9000
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-  ingress {
-    description      = "HTTP"
-    from_port        = 3000
-    to_port          = 3000
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name        = "service-sg"
-    Environment = var.environment
   }
 }
