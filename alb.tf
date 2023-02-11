81a431824696a026eece37823af83369963244fa
kaanozbudak <kaan.ozbudak@ametrica.io>
2023-02-11 23:49:56 +0300

    removed unused services

--- alb.tf
+++ alb.tf
@@ -87,38 +87,6 @@ resource "aws_security_group" "allow-http-sg" {
 }
 /////////////////////////
 
-
-//------------- backend services ----------
-//alb
-resource "aws_lb" "backend-alb" {
-  name               = "backend-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = ["sg-09d6376212dfa6ea1"] // Todo change
-  subnets            = [aws_subnet.public-subnet-a.id, aws_subnet.public-subnet-b.id]
-
-  enable_deletion_protection = true
-
-  tags = {
-    Name = "backend-alb"
-  }
-}
-
-//listener 
-resource "aws_lb_listener" "backend-alb-listener" {
-  load_balancer_arn = aws_lb.backend-alb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.api-tg.arn
-  }
-  depends_on = [
-    aws_lb.backend-alb
-  ]
-}
-
 //------------- backend services ----------
 
 
