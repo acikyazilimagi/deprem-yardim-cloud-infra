6ccf8163c8e0fa5ca59806ac74220dbb0ee29f1a
kaanozbudak <kaan.ozbudak@ametrica.io>
2023-02-11 19:51:02 +0300

    ecs tf

---
+++ ecs.tf
@@ -0,0 +1,209 @@
+data "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsServiceRole"
+}
+
+resource "aws_ecs_cluster" "base-cluster" {
+  name = "base-cluster"
+  tags = {
+    Name        = "base-cluster"
+    Environment = var.environment
+  }
+}
+
+
+
+resource "aws_security_group" "service-sg" {
+  name        = "service-sg"
+  description = "Allow HTTP/HTTPS inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    description      = "HTTP"
+    from_port        = 9000
+    to_port          = 9000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    description      = "HTTP"
+    from_port        = 3000
+    to_port          = 3000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name        = "service-sg"
+    Environment = var.environment
+  }
+}
+
+
+
+//worker
+resource "aws_ecs_task_definition" "worker-TD" {
+  family                   = "worker-TD"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 512
+  memory                   = 1024
+  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
+  container_definitions = jsonencode([
+    {
+      name   = "container-name"
+      image  = "nginx"
+      cpu    = 512
+      memory = 1024
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-create-group  = "true"
+          awslogs-group         = "/ecs/worker"
+          awslogs-region        = var.region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+      essential = true
+    }
+  ])
+
+}
+
+resource "aws_ecs_service" "worker-service" {
+  name            = "worker-service"
+  cluster         = aws_ecs_cluster.base-cluster.id
+  task_definition = aws_ecs_task_definition.worker-TD.id
+  desired_count   = 1
+  depends_on = [
+    aws_ecs_cluster.base-cluster,
+    aws_ecs_task_definition.worker-TD,
+  ]
+  launch_type = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.private-subnet-a.id, aws_subnet.private-subnet-b.id]
+    security_groups  = [aws_security_group.service-sg.id]
+    assign_public_ip = true
+  }
+}
+
+//worker
+
+
+//web api
+
+
+resource "aws_ecs_task_definition" "api-TD" {
+  family                   = "api-TD"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 2048
+  memory                   = 4096
+  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
+  container_definitions = jsonencode([
+    {
+      name   = "container-name"
+      image  = "nginx" //bunu düzelticem
+      cpu    = 2048
+      memory = 4096
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-create-group  = "true"
+          awslogs-group         = "/ecs/api"
+          awslogs-region        = var.region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_lb_target_group" "api-tg" {
+  name        = "api-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.vpc.id
+  health_check {
+    enabled  = true
+    path     = "/core/health/"
+    port     = 80
+    protocol = "HTTP"
+  }
+  tags = {
+    Name        = "api-tg"
+    Environment = var.environment
+  }
+}
+
+resource "aws_ecs_service" "api-service" {
+  name            = "api-service"
+  cluster         = aws_ecs_cluster.base-cluster.id
+  task_definition = aws_ecs_task_definition.api-TD.id
+  desired_count   = 10
+  depends_on = [
+    aws_ecs_cluster.base-cluster,
+    aws_ecs_task_definition.api-TD,
+    aws_lb_target_group.api-tg,
+  ]
+  launch_type = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.private-subnet-a.id, aws_subnet.private-subnet-b.id]
+    security_groups  = [aws_security_group.service-sg.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.api-tg.arn
+    container_name   = "container-name"
+    container_port   = 80
+  }
+}
+
+
+
+resource "aws_lb_listener_rule" "api-rule" {
+  listener_arn = aws_lb_listener.backend-alb-listener.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.api-tg.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+}
+// web api
