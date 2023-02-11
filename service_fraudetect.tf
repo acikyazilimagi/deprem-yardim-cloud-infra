e7954aede24d8c014502b89b853bce5e9e892290
Emre Erkunt <emre.erkunt@gmail.com>
2023-02-11 22:05:22 +0000

    Add service_fraudetect.tf

---
+++ service_fraudetect.tf
@@ -0,0 +1,124 @@
+
+//web api
+
+
+resource "aws_ecs_task_definition" "fraudetect-TD" {
+  family                   = "fraudetect-TD"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 2048
+  memory                   = 4096
+  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
+  container_definitions = jsonencode([
+    {
+      name   = "container-name"
+      image  = "fraudetect" //bunu düzelticem
+      cpu    = 2048
+      memory = 4096
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-create-group  = "true"
+          awslogs-group         = "/ecs/fraudetect"
+          awslogs-region        = var.region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_lb_target_group" "fraudetect-tg" {
+  name        = "fraudetect-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.vpc.id
+  health_check {
+    enabled  = true
+    path     = "/"
+    port     = 80
+    protocol = "HTTP"
+  }
+  tags = {
+    Name        = "fraudetect-tg"
+    Environment = var.environment
+  }
+}
+
+resource "aws_ecs_service" "fraudetect-service" {
+  name            = "fraudetect-service"
+  cluster         = aws_ecs_cluster.base-cluster.id
+  task_definition = aws_ecs_task_definition.fraudetect-TD.id
+  desired_count   = 2
+  depends_on = [
+    aws_ecs_cluster.base-cluster,
+    aws_ecs_task_definition.fraudetect-TD,
+    aws_lb_target_group.fraudetect-tg,
+  ]
+  launch_type = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.private-subnet-a.id, aws_subnet.private-subnet-b.id]
+    security_groups  = [aws_security_group.service-sg.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.fraudetect-tg.arn
+    container_name   = "container-name"
+    container_port   = 80
+  }
+}
+
+
+resource "aws_lb_listener_rule" "fraudetect-rule" {
+  listener_arn = aws_lb_listener.fraudetect-alb-listener.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.fraudetect-tg.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+}
+
+resource "aws_lb" "fraudetect-alb" {
+  name               = "fraudetect-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["sg-09d6376212dfa6ea1"] // Todo change
+  subnets            = [aws_subnet.public-subnet-a.id, aws_subnet.public-subnet-b.id]
+
+  enable_deletion_protection = true
+
+  tags = {
+    Name = "fraudetect-alb"
+  }
+}
+
+resource "aws_lb_listener" "fraudetect-alb-listener" {
+  load_balancer_arn = aws_lb.fraudetect-alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.fraudetect-tg.arn
+  }
+  depends_on = [
+    aws_lb.fraudetect-alb
+  ]
+}
