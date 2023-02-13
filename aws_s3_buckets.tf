088d35697612b3f3a2203cf2b2db03a95fcfee99
Burak Cinar <burakcinar@192.168.1.79>
2023-02-13 20:27:56 +0300

    s3 bucket fix

--- aws_s3_buckets.tf
+++ aws_s3_buckets.tf
@@ -1,13 +1,4 @@
 resource "aws_s3_bucket" "buckets" {
   for_each = toset( ["aws-waf-logs-afetorg", "afet-logs-alb", "afet-logs-cloudtrail", "afet-logs-vpcflowlogs"] )
   bucket = each.key
-  object_lock_configuration {
-    object_lock_enabled = "Enabled"
-    rule {
-      default_retention {
-        mode = "COMPLIANCE"
-        days = 45
-      }
-    }
-  }
 }
