fb1313c30bb1fd4a8da530ba07add97d090d92e7
Burak Cinar <iamburakcinar@gmail.com>
2023-02-14 10:49:01 +0300

    s3 bucket block public

--- aws_s3_buckets.tf
+++ aws_s3_buckets.tf
@@ -2,4 +2,9 @@ resource "aws_s3_bucket" "buckets" {
   for_each = toset(["aws-waf-logs-afetorg", "afet-logs-alb", "afet-logs-cloudtrail", "afet-logs-vpcflowlogs"])
   bucket   = each.key
   acl      = "private"
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
