87effa0bf5e0224e34482caf76316e011e4ca7f9
Ferhat BOSTANCI <ferhatbostanciofficial@gmail.com>
2023-02-16 21:06:53 +0300

    go-consumer sg update

--- service_go-consumer.tf
+++ service_go-consumer.tf
@@ -38,7 +38,7 @@ resource "aws_ecs_service" "go-consumer-service" {
 
   network_configuration {
     subnets          = [aws_subnet.private-subnet-a.id, aws_subnet.private-subnet-b.id]
-    security_groups  = [aws_security_group.allow-http-sg.id]
+    security_groups  = [aws_security_group.service-sg.id]
     assign_public_ip = true
   }
 
