cca4c6ef5c1b90d8fc596574f4f019d2487ebf10
Burak CINAR <5022026+iamburakcinar@users.noreply.github.com>
2023-02-17 12:58:42 +0300

    Delete aws_s3_buckets.tf

--- aws_s3_buckets.tf
+++
@@ -1,9 +0,0 @@
-# resource "aws_s3_bucket" "buckets" {
-#   for_each = toset(["aws-waf-logs-afetorg", "afet-logs-alb", "afet-logs-cloudtrail", "afet-logs-vpcflowlogs"])
-#   bucket   = each.key
-
-#   block_public_acls       = true
-#   block_public_policy     = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
-# }
