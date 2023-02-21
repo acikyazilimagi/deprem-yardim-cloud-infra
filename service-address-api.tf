c3f840286262c715e621ad09afc5a7f27e6c147b
kaanozbudak <kaanozbudak@hotmail.com>
2023-02-21 17:55:27 +0300

    depremio-service-address added

--- service-address-api.tf
+++ service-address-api.tf
@@ -246,3 +246,53 @@ resource "aws_ecs_service" "address-data-excell-service" {
     ignore_changes = [task_definition]
   }
 }
+
+
+resource "aws_ecs_task_definition" "address-depremio-TD" {
+  family                   = "address-depremio-TD"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 512
+  memory                   = 1024
+  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
+  container_definitions = jsonencode([
+    {
+      name   = "container-name"
+      image  = "nginx"
+      cpu    = 512
+      memory = 1024
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-create-group  = "true"
+          awslogs-group         = "/ecs/address-depremio"
+          awslogs-region        = var.region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+      essential = true
+    }
+  ])
+}
+
+resource "aws_ecs_service" "address-depremio-service" {
+  name            = "address-depremio-service"
+  cluster         = aws_ecs_cluster.base-cluster.id
+  task_definition = aws_ecs_task_definition.address-depremio-TD.id
+  desired_count   = 1
+  depends_on = [
+    aws_ecs_cluster.base-cluster,
+    aws_ecs_task_definition.address-depremio-TD,
+  ]
+  launch_type = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.private-subnet-a.id, aws_subnet.private-subnet-b.id]
+    security_groups  = [aws_security_group.service-sg.id]
+    assign_public_ip = true
+  }
+
+  lifecycle {
+    ignore_changes = [task_definition]
+  }
+}
